8cde275d57c4f16e45efa38d53625f11f9cc2a72
Dylan Gabriel <dylansaucedo85@gmail.com>
2023-03-22 15:26:30 -0600

    Primera grafica

--- Teoria-de-graficas/Grafo.py
+++ Teoria-de-graficas/Grafo.py
@@ -15,3 +15,4 @@ pos=nx.spring_layout(G)
 nx.draw(G,pos,with_labels=True)
 nx.draw_networkx_edge_labels(G,pos,edge_labels={('V3','V2'):'e5',('V3','V1'):'e4',('V3','V4'):'e1',\
                                                 ('V2','V1'):'e3',('V1','V4'):'e2'}, font_color='black')
+plt.show()
